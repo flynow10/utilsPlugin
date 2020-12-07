325473a7568358ed71694eb64f92233167fbf1ff
Nate Wagner <nate.o.wagner@gmail.com>
2020-12-07 15:51:02 -0500

    Make Buildable 2

--- src/main/java/com/wagologies/utilsplugin/commands/Ban.java
+++ src/main/java/com/wagologies/utilsplugin/commands/Ban.java
@@ -2,7 +2,6 @@ package com.wagologies.utilsplugin.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class Ban extends CommandBase {
--- src/main/java/com/wagologies/utilsplugin/utils/DisplayPackets.java
+++ src/main/java/com/wagologies/utilsplugin/utils/DisplayPackets.java
@@ -4,7 +4,6 @@ import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
-import org.apache.commons.lang.StringEscapeUtils;
 import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
